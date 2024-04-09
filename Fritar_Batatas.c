ce4cda5293e1d2acb2babfcd4a20fc7f372923ad
LucasDNiguti <166518261+LucasDNiguti@users.noreply.github.com>
2024-04-09 14:00:10 +0000

    01 - 09/04/2024 11:00

---
+++ Fritar_Batatas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void) {
+
+    for (int batata = 0; batata < 1; batata++){
+        rand()%1;
+    }
+
+
+    if(batata = 1){
+        prinf("Cortar Batata\n");
+        printf("Pegar frigideira\n");
+        printf("Colocar oleo dentro da frigideira\n");
+        printf("Esquentar o oleo\n")
+        printf("Fritar a batata\n");
+    } else {
+        printf("Sem Batata")
+    }
+    return 0;
+}
